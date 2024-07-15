14f57451b4fbb8c8b0de75acc5d8f3f475931faa
mrprotocoll <lekanvgbg@gmail.com>
2024-07-15 10:25:10 +0100

    update faq

--- intro/faq.js
+++ intro/faq.js
@@ -1,10 +1,8 @@
 const faqItem = document.querySelectorAll(".faq-item");
 const faqItems = document.getElementById('faq-items');
-const faqEditButton = document.querySelectorAll(".edit");
-const faqHeading = document.getElementsByClassName('faq-heading');
 const form = document.getElementById("faq-form");
 const editForm = document.getElementById("edit-faq-form");
-
+let currentEditingFaq = null;
 let faqs = [];
 
 const persistedFaq = JSON.parse(localStorage.getItem("faqs"));
@@ -16,38 +14,36 @@ if(persistedFaq) {
 }
 
 faqItem.forEach(function (item){
-    item.addEventListener('click', toggleFaq)
+    item.addEventListener('click', function (element){
+        toggleFaq(item)
+    })
 })
 
-document.body.addEventListener('click', function (e) {
+faqItems.addEventListener('click', function (e) {
     e.preventDefault();
 
     if(e.target.className === 'edit'){
         const button = e.target;
+
         const heading = button.previousElementSibling.firstElementChild;
+        currentEditingFaq = heading;
         const inputFiled =  document.getElementById('editQuestion');
         inputFiled.value = heading.innerText;
-
-        document.getElementById("edit-faq-form").addEventListener('submit', function (e){
-            // e.preventDefault();
-            console.log("updated");
-            heading.innerText = this.editQuestion.value;
-            this.reset();
-        })
+    }else if(e.target.className === 'delete'){
+        e.target.parentElement.remove();
     }
 })
 
 // update faq form
 editForm.addEventListener('submit', function (e){
     e.preventDefault();
-    const question = this.editQuestion.value;
-
-    const index = this.getAttribute('data-id');
-    faqHeading[index].innerText = question;
-
-    // clear input and form
-    editForm.reset();
-    this.removeAttribute('data-id');
+    if(currentEditingFaq) {
+        currentEditingFaq.innerText = this.editQuestion.value;
+        // clear input and form
+        editForm.reset();
+    }else{
+        alert("Please select faq to update");
+    }
 })
 
 // Add Faq
@@ -70,31 +66,22 @@ form.addEventListener('submit', function (e){
 function appendFaq(question, answer) {
     const heading = document.createElement('div');
     heading.classList.add("heading");
-    const newFaqItem = document.createElement('div');
-    const editButton = document.createElement('button');
-    editButton.classList.add("edit");
-    editButton.innerText = 'Edit';
-    heading.append(newFaqItem);
-    heading.append(editButton);
-    newFaqItem.classList.add('faq-item');
-    newFaqItem.innerHTML = `<h3 class="faq-heading">${question}</h3><p>${answer}</p>`;
-    newFaqItem.addEventListener('click', toggleFaq);
+    heading.innerHTML = faqHtml(question, answer);
     faqItems.append(heading);
-    // editButton.addEventListener('click', function (e){
-    //     e.preventDefault();
-    //     clickEditButton(question);
-    // });
+    heading.addEventListener('click', function (element){
+        toggleFaq(element.target.parentElement)
+    });
 }
 
 function faqHtml(question, answer) {
     return `
-        <div class="heading">
-            <div class="faq-item">
-                <h3 class="faq-heading">${question}</h3>
-                <p>You can include JavaScript in your web page by using the tag. </p>
-            </div>
-            <button class="edit">Edit</button>
-        </div>`
+        <div class="faq-item">
+            <h3 class="faq-heading">${question}</h3>
+            <p>You can include JavaScript in your web page by using the tag. </p>
+        </div>
+        <button class="edit">Edit</button>
+        <button class="delete">Delete</button>
+        `
 }
 
 function toggleFaq(element) {
@@ -103,15 +90,5 @@ function toggleFaq(element) {
             item.classList.remove('active')
         }
     })
-    element.currentTarget.classList.toggle('active');
+    element.classList.toggle('active');
 }
-
-function clickEditButton(index) {
-    const inputFiled =  document.getElementById('editQuestion');
-    inputFiled.value = (typeof(index) == 'number') ? faqHeading[index].innerText : index;
-
-    // add the heading id to the form data-id for easy update
-    editForm.setAttribute('data-id', index);
-}
-
-// Event bubbling
